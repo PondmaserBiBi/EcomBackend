381b389868e40dfb8064077f84ea2b16b60d3af8
PondBiBi <pondmaserbibi@gmail.com>
2025-12-07 21:28:16 +0700

    first commit

---
+++ controllers/stripe.js
@@ -0,0 +1,41 @@
+const prisma = require("../config/prisma")
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+
+exports.payment = async (req, res) => {
+
+    try {
+
+        const cart = await prisma.cart.findFirst({
+
+            where: {
+
+                orderById: req.user.id
+            }
+
+        })
+
+        const amountTHB = cart.cartTotal * 100
+
+
+
+        const paymentIntent = await stripe.paymentIntents.create({
+
+            amount: amountTHB,
+            currency: 'thb',
+
+            automatic_payment_methods: {
+                enabled: true,
+            },
+        })
+
+        res.send({
+            clientSecret: paymentIntent.client_secret,
+        })
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Server Error' })
+
+    }
+}
